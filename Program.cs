e8b8959bf3027982aed964c006dd41e5bed19277
Steffen Skov <steffen@drofus.com>
2023-01-31 11:39:16 +0100

    Polish console output

--- Program.cs
+++ Program.cs
@@ -4,35 +4,20 @@
 using System.Collections.Concurrent;
 
 Console.Clear();
-
-// var workIds = new int[]
-// {
-//     5, 3, 9, 6, 2, 8, 7, 1, 0, 4
-// };
-//
-// var rand = new Random();
-// for (int j = 0; j < 5; j++)
-// {
-//     for (int i = 0; i < 10; i++)
-//     {
-//         var pct = rand.Next(0, 100);
-//         var pr = new FileProgress(workIds[i], pct);
-//         ReportProgress(pr);
-//     }
-// }
+Console.CursorVisible = false;
 
 BlockingCollection<FileProgress> progressEvents = new BlockingCollection<FileProgress>();
 
-var timer = new Timer(ProcessReports, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(500));
+var timer = new Timer(ProcessReports, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(200));
 
 var processor = new FileProcessor();
 var progress = new ConsoleProgress<FileProgress>(p => ReportProgress(p));
 int result = await processor.ProcessFiles(10, progress);
 
 await Task.Delay(TimeSpan.FromMilliseconds(200));
-
 Console.SetCursorPosition(0,17);
 Console.WriteLine($"Result of files processed: {result}");
+Console.CursorVisible = true;
 
 
 void ReportProgress(FileProgress fileProgress)
@@ -51,13 +36,15 @@ void ProcessReports(Object whatever)
 void ProcessReport(FileProgress progress)
 {
     Console.SetCursorPosition(0, 5 + progress.WorkId);
-    Console.Write($"{progress.WorkId}:");
+    Console.Write($"{progress.WorkId} {progress.PctComplete:000}%: ");
+    var progressChar = progress.PctComplete < 100 ? "-" : "#";
     for (int i = 0; i < progress.PctComplete / 10; i++)
     {
-        Console.Write(".");
+        Console.Write(progressChar);
     }
 
-    Console.Write($" {progress.PctComplete}% done");
-
-    // Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] {fileProgress.WorkId}: {fileProgress.PctComplete}% done");
+    if (progress.PctComplete < 100)
+    {
+        Console.Write(">");
+    }
 }
